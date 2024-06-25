f815b45a6d1567e9b6410e87fa4be246b3b8747e
Luciana Urristi <lu_comu@hotmail.com>
2024-06-25 19:17:21 -0300

    modificaciones

--- js/limpiarCampos.js
+++ js/limpiarCampos.js
@@ -2,15 +2,10 @@ const formulario = document.querySelector("[data-formulario]");
 const limpiar = document.getElementById('limpiar');
 
 function limpiarCampos() {
-    console.log("Click en el botón 'Limpiar'"); // Verifica si se registra el evento click
-
-    // Obtén todos los elementos con la clase '.campo__escrita'
     const campos = document.querySelectorAll('.campo__escrita');
-    console.log("Número de campos encontrados:", campos.length); // Verifica la cantidad de campos encontrados
 
     // Itera sobre cada campo y limpia su valor
     campos.forEach(function (input) {
-        console.log(input); // Para verificar que estás obteniendo los campos correctamente
         input.value = '';
     });
 }
